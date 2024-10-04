f8828ecbf5949a1a6d886d1e80f5413980ad5aa9
abi-2006 <abishastjoseph27@gmail.com>
2024-10-04 16:08:17 +0530

    Add files via upload

---
+++ testrun.py
@@ -0,0 +1,61 @@
+'''print("hello, World")
+print("hello\nWorld"
+      
+
+if 5>2:
+    print("five is greater than two")
+if 6>2:
+    print("six greater than two")
+if 5>2:
+ print ("five greater than two"
+        
+        
+        
+
+x=6
+y="appu"
+print(x)
+print(y)
+x= 6
+x="tiya"
+print(x)
+
+
+
+x=str(3)# x will be '3'
+y=int(3)# y will bw 3
+z=float(3)# z will be 3.0
+print (x)
+y="jhon"
+print(type(x))
+print(type(y))
+
+
+
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+
+
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+
+
+x="python "
+y="is "
+z="awesome"
+print(x+y+z)
+
+
+username = input("enter username:")
+print("username is: "+ username)
+'''
+
+a="hello"
+b="world"
+c=(a+b)
+print(c)
