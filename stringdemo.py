9ed93315e18860420f7162cffd6de0766541c710
abi-2006 <abishastjoseph27@gmail.com>
2024-10-18 15:09:55 +0530

    Add files via upload

--- stringdemo.py
+++ stringdemo.py
@@ -1,3 +1,6 @@
+'''
+Author;Abisha Biju
+program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string
 str_var="Hello, world!"
 str2_var=str_var[7:12]
 print(str2_var)
