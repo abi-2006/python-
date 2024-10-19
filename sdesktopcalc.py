2b47f3fb8cbd4771bd0a1994b33e0592e2bc31dd
abi-2006 <abishastjoseph27@gmail.com>
2024-10-19 14:50:14 +0530

    Add files via upload

---
+++ sdesktopcalc.py
@@ -0,0 +1,19 @@
+'''
+Author:Abisha Biju
+a Python program that performs the following tasks
+'''
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+num3=int(input("enter num3:"))
+sum=num1+num2
+print("sum of num1 and num2 is:",sum)
+difference=num2-num1
+print("The difference when num2 is substracted from num1 is:",difference)
+product=num1*num2
+print("the product of num1 amd num2 is :",product)
+quotient=num1/num2
+print("the quotient when num1 divided by num2 is:",quotient)
+modulus=num1%num2
+print("the remainder when num1 id divided by num 2 is :",modulus)
+result=(num1 + num2) * num3/2
+print("the result of num1+num2 * num3/2:",result)
