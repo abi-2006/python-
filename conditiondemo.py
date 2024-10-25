e35eef529ab24d76157d4cdada0e4a6d05b8b2a8
abi-2006 <abishastjoseph27@gmail.com>
2024-10-25 14:18:38 +0530

    Add files via upload

---
+++ conditiondemo.py
@@ -0,0 +1,11 @@
+'''
+Author:Abisha Biju
+program to find the largest of three numbers. The program should take three numbers as input from the user and determine which one is the largest. Use conditional statements to compare the numbers.
+'''
+number1=int(input("enter the first number:"))
+number2=int(input("enter the second number:"))
+if(number1>number2):
+    print(number1,"is greater than",number2)
+else:
+    print(number2,"is greater than",number1)
+    print("the remaining statements")
